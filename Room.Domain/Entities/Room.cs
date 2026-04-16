3d91461593a157970b58b831641a91ee752ef405
gplacenti <gplacenti>
2026-04-16 10:34:52 +0200

    Add missing enity room

---
+++ Room.Domain/Entities/Room.cs
@@ -0,0 +1,23 @@
+﻿namespace BookingRoom.Domain.Entities;
+
+public class Room
+{
+    public int Id { get; private set; }
+    public string Name { get; init; }
+    public int Capacity { get; init; }
+    public List<Booking> Bookings { get; set; } = [];
+
+    public Room() { }
+
+    public Room(string name, int capacity, int? id = null)
+    {
+        Name = name;
+        Capacity = capacity;
+        if (id.HasValue)
+            Id = id.Value;
+    }
+
+
+    public bool CanBook(Booking newBooking)
+        => !Bookings.Any(b => newBooking.ConflictsWith(b));
+}
